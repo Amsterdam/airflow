005687a2f45643120eb2780d09ff4419bf7855ee
Stephan Zaat <s.zaat@amsterdam.nl>
2021-02-16 11:17:56 -0400

    performance_tiling

--- dags/tiles.py
+++ dags/tiles.py
@@ -62,26 +62,26 @@ with DAG(
     start_date=days_ago(2),
     tags=['tiles'],
 ) as dag:
-    trex_generate_pbf_wm = KubernetesPodOperator(
-        name="trex_generate_pbf_wm",
-        labels={"aadpodidbinding": "pio-tiles-id"},
-        image="sourcepole/t-rex",
-        namespace="tiles",
-        arguments=["generate", "--maxzoom", "16", "--config", "/var/config/topo_wm.toml"],
-        task_id="trex_generate_pbf_wm",
-        volumes=[volume_config_trex, volume_data_trex],
-        volume_mounts=[volume_mount_config, volume_mount_data_trex],
-        security_context=dict(fsGroup=33),
-        configmaps=trex_configmaps,
-        get_logs=True,
-        do_xcom_push=False
-    )
+    # trex_generate_pbf_wm = KubernetesPodOperator(
+    #     name="trex_generate_pbf_wm",
+    #     labels={"aadpodidbinding": "pio-tiles-id"},
+    #     image="sourcepole/t-rex",
+    #     namespace="tiles",
+    #     arguments=["generate", "--minzoom", "10", "--maxzoom", "16", "--extent", "4.49712476945351,52.1630507756721,5.60867873764429,52.6147675426215", "--config", "/var/config/topo_wm.toml"],
+    #     task_id="trex_generate_pbf_wm",
+    #     volumes=[volume_config_trex, volume_data_trex],
+    #     volume_mounts=[volume_mount_config, volume_mount_data_trex],
+    #     security_context=dict(fsGroup=33),
+    #     configmaps=trex_configmaps,
+    #     get_logs=True,
+    #     do_xcom_push=False
+    # )
     # trex_generate_pbf_rd = KubernetesPodOperator(
     #     name="trex_generate_pbf_rd",    
     #     labels={"aadpodidbinding": "pio-tiles-id"},
     #     image="sourcepole/t-rex",
     #     namespace="tiles",
-    #     arguments=["generate", "--maxzoom", "11", "--config", "/var/config/topo_rd.toml"],
+    #     arguments=["generate", "--minzoom", "5", "--maxzoom", "11", "--extent", "4.49712476945351,52.1630507756721,5.60867873764429,52.6147675426215", "--config", "/var/config/topo_rd.toml"],
     #     task_id="trex_generate_pbf_rd",
     #     volumes=[volume_config_trex, volume_data_trex],
     #     volume_mounts=[volume_mount_config, volume_mount_data_trex],
@@ -90,20 +90,20 @@ with DAG(
     #     get_logs=True,
     #     do_xcom_push=False
     # )
-    upload_pbf_wm = KubernetesPodOperator(
-        name="upload_pbf_wm",
-        labels={"aadpodidbinding": "pio-tiles-id"},
-        image="hawaku/azcopy",
-        namespace="tiles",
-        cmds=["/bin/bash"],
-        arguments=["-c", "azcopy login --identity --identity-client-id 60efcd71-1ca4-4650-ba7b-66f04c720d75; azcopy copy '/var/cache/mvtcache/wm/*' https://piosupportstor.blob.core.windows.net/tiles/wm/pbf/ --recursive --content-encoding gzip"],
-        task_id="upload_pbf_wm",
-        volumes=[volume_data_trex],
-        volume_mounts=[volume_mount_data_trex],
-        security_context=dict(fsGroup=101),
-        get_logs=True,
-        do_xcom_push=False
-    )
+    # upload_pbf_wm = KubernetesPodOperator(
+    #     name="upload_pbf_wm",
+    #     labels={"aadpodidbinding": "pio-tiles-id"},
+    #     image="hawaku/azcopy",
+    #     namespace="tiles",
+    #     cmds=["/bin/bash"],
+    #     arguments=["-c", "azcopy login --identity --identity-client-id 60efcd71-1ca4-4650-ba7b-66f04c720d75; azcopy copy '/var/cache/mvtcache/wm/*' https://piosupportstor.blob.core.windows.net/tiles/wm/pbf/ --recursive --content-encoding gzip"],
+    #     task_id="upload_pbf_wm",
+    #     volumes=[volume_data_trex],
+    #     volume_mounts=[volume_mount_data_trex],
+    #     security_context=dict(fsGroup=101),
+    #     get_logs=True,
+    #     do_xcom_push=False
+    # )
     # upload_pbf_rd = KubernetesPodOperator(
     #     name="upload_pbf_rd",
     #     labels={"aadpodidbinding": "pio-tiles-id"},
@@ -275,10 +275,13 @@ with DAG(
     #     do_xcom_push=False
     # )
 
-trex_generate_pbf_wm >> upload_pbf_wm
-upload_pbf_wm >> mapproxy_generate_tiles_wm >> upload_tiles_wm
-upload_pbf_wm >> mapproxy_generate_tiles_wm_zw >> upload_tiles_wm_zw
-upload_pbf_wm >> mapproxy_generate_tiles_wm_light >> upload_tiles_wm_light 
+mapproxy_generate_tiles_wm >> upload_tiles_wm
+mapproxy_generate_tiles_wm_zw >> upload_tiles_wm_zw
+mapproxy_generate_tiles_wm_light >> upload_tiles_wm_light 
+# trex_generate_pbf_wm >> upload_pbf_wm
+# upload_pbf_wm >> mapproxy_generate_tiles_wm >> upload_tiles_wm
+# upload_pbf_wm >> mapproxy_generate_tiles_wm_zw >> upload_tiles_wm_zw
+# upload_pbf_wm >> mapproxy_generate_tiles_wm_light >> upload_tiles_wm_light 
 # trex_generate_pbf_rd >> upload_pbf_rd
 # upload_pbf_rd >> mapproxy_generate_tiles_rd >> upload_tiles_rd 
 # upload_pbf_rd >> mapproxy_generate_tiles_rd_zw >> upload_tiles_rd_zw
