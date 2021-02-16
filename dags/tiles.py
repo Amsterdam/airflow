144f87cb3fc7163dc4047ac937b4f99c9c7efad4
Stephan Zaat <s.zaat@amsterdam.nl>
2021-02-16 19:38:35 -0400

    more_resources

--- dags/tiles.py
+++ dags/tiles.py
@@ -56,6 +56,8 @@ volume_config_mapproxy = Volume(name='volume-cm', configs=volume_cm_mapproxy)
 volume_data_trex = Volume(name='trex-volume-data', configs=volume_dt_trex)
 volume_data_mapproxy = Volume(name='mapproxy-volume-data', configs=volume_dt_mapproxy)
 
+fullresources = {'request_memory': '6Gi', 'request_cpu': '2500m', 'limit_memory': '12Gi', 'limit_cpu': '3000m'}
+
 with DAG(
     dag_id='tiles',
     schedule_interval=None,
@@ -76,20 +78,23 @@ with DAG(
     #     get_logs=True,
     #     do_xcom_push=False
     # )
-    # trex_generate_pbf_rd = KubernetesPodOperator(
-    #     name="trex_generate_pbf_rd",    
-    #     labels={"aadpodidbinding": "pio-tiles-id"},
-    #     image="sourcepole/t-rex",
-    #     namespace="tiles",
-    #     arguments=["generate", "--minzoom", "5", "--maxzoom", "11", "--extent", "4.49712476945351,52.1630507756721,5.60867873764429,52.6147675426215", "--config", "/var/config/topo_rd.toml"],
-    #     task_id="trex_generate_pbf_rd",
-    #     volumes=[volume_config_trex, volume_data_trex],
-    #     volume_mounts=[volume_mount_config, volume_mount_data_trex],
-    #     security_context=dict(fsGroup=33),
-    #     configmaps=trex_configmaps,
-    #     get_logs=True,
-    #     do_xcom_push=False
-    # )
+    trex_generate_pbf_rd = KubernetesPodOperator(
+        name="trex_generate_pbf_rd",    
+        labels={"aadpodidbinding": "pio-tiles-id"},
+        image="sourcepole/t-rex",
+        namespace="tiles",
+        arguments=["generate", "--minzoom", "5", "--maxzoom", "11", "--extent", "4.49712476945351,52.1630507756721,5.60867873764429,52.6147675426215", "--config", "/var/config/topo_rd.toml"],
+        task_id="trex_generate_pbf_rd",
+        volumes=[volume_config_trex, volume_data_trex],
+        volume_mounts=[volume_mount_config, volume_mount_data_trex],
+        security_context=dict(fsGroup=33),
+        configmaps=trex_configmaps,
+        node_selectors={"nodetype": "tiles"},
+        is_delete_operator_pod=False,
+        resources=fullresources,
+        get_logs=True,
+        do_xcom_push=False
+    )
     # upload_pbf_wm = KubernetesPodOperator(
     #     name="upload_pbf_wm",
     #     labels={"aadpodidbinding": "pio-tiles-id"},
@@ -104,20 +109,21 @@ with DAG(
     #     get_logs=True,
     #     do_xcom_push=False
     # )
-    # upload_pbf_rd = KubernetesPodOperator(
-    #     name="upload_pbf_rd",
-    #     labels={"aadpodidbinding": "pio-tiles-id"},
-    #     image="hawaku/azcopy",
-    #     namespace="tiles",
-    #     cmds=["/bin/bash"],
-    #     arguments=["-c", "azcopy login --identity --identity-client-id 60efcd71-1ca4-4650-ba7b-66f04c720d75; azcopy copy '/var/cache/mvtcache/rd/*' https://piosupportstor.blob.core.windows.net/tiles/rd/pbf/ --recursive --content-encoding gzip"],
-    #     task_id="upload_pbf_rd",
-    #     volumes=[volume_data_trex],
-    #     volume_mounts=[volume_mount_data_trex],
-    #     security_context=dict(fsGroup=101),
-    #     get_logs=True,
-    #     do_xcom_push=False
-    # )    
+    upload_pbf_rd = KubernetesPodOperator(
+        name="upload_pbf_rd",
+        labels={"aadpodidbinding": "pio-tiles-id"},
+        image="hawaku/azcopy",
+        namespace="tiles",
+        cmds=["/bin/bash"],
+        arguments=["-c", "azcopy login --identity --identity-client-id 60efcd71-1ca4-4650-ba7b-66f04c720d75; azcopy copy '/var/cache/mvtcache/rd/*' https://piosupportstor.blob.core.windows.net/tiles/rd/pbf/ --recursive --content-encoding gzip"],
+        task_id="upload_pbf_rd",
+        volumes=[volume_data_trex],
+        volume_mounts=[volume_mount_data_trex],
+        security_context=dict(fsGroup=101),
+        is_delete_operator_pod=False,
+        get_logs=True,
+        do_xcom_push=False
+    )    
     mapproxy_generate_tiles_wm = KubernetesPodOperator(
         name="mapproxy_generate_tiles_wm",
         image="dsoapi.azurecr.io/mapproxy",
@@ -129,34 +135,40 @@ with DAG(
         security_context=dict(fsGroup=101),
         node_selectors={"nodetype": "tiles"},
         is_delete_operator_pod=True,
-        resources={'request_memory': '6Gi', 'request_cpu': '2500m', 'limit_memory': '12Gi', 'limit_cpu': '3000m'},
+        resources=fullresources,
         get_logs=True,
         do_xcom_push=False
     )
-    # mapproxy_generate_tiles_wm_zw = KubernetesPodOperator(
-    #     name="mapproxy_generate_tiles_wm_zw",
-    #     image="dsoapi.azurecr.io/mapproxy",
-    #     namespace="tiles",
-    #     arguments=["mapproxy-seed", "-c", "2", "-s", "/var/config/seed.yaml", "-f", "/var/config/mapproxy.yaml", "--seed=wm_kbk_zw"],
-    #     task_id="mapproxy_generate_tiles_wm_zw",
-    #     volumes=[volume_config_mapproxy, volume_data_mapproxy],
-    #     volume_mounts=[volume_mount_config, volume_mount_data_mapproxy],
-    #     security_context=dict(fsGroup=101),
-    #     get_logs=True,
-    #     do_xcom_push=False
-    # )
-    # mapproxy_generate_tiles_wm_light = KubernetesPodOperator(
-    #     name="mapproxy_generate_tiles_wm_light",
-    #     image="dsoapi.azurecr.io/mapproxy",
-    #     namespace="tiles",
-    #     arguments=["mapproxy-seed", "-c", "2", "-s", "/var/config/seed.yaml", "-f", "/var/config/mapproxy.yaml", "--seed=wm_kbk_light"],
-    #     task_id="mapproxy_generate_tiles_wm_light",
-    #     volumes=[volume_config_mapproxy, volume_data_mapproxy],
-    #     volume_mounts=[volume_mount_config, volume_mount_data_mapproxy],
-    #     security_context=dict(fsGroup=101),
-    #     get_logs=True,
-    #     do_xcom_push=False
-    # )
+    mapproxy_generate_tiles_wm_zw = KubernetesPodOperator(
+        name="mapproxy_generate_tiles_wm_zw",
+        image="dsoapi.azurecr.io/mapproxy",
+        namespace="tiles",
+        arguments=["mapproxy-seed", "-c", "2", "-s", "/var/config/seed.yaml", "-f", "/var/config/mapproxy.yaml", "--seed=wm_kbk_zw"],
+        task_id="mapproxy_generate_tiles_wm_zw",
+        volumes=[volume_config_mapproxy, volume_data_mapproxy],
+        volume_mounts=[volume_mount_config, volume_mount_data_mapproxy],
+        security_context=dict(fsGroup=101),
+        node_selectors={"nodetype": "tiles"},
+        is_delete_operator_pod=True,
+        resources=fullresources,
+        get_logs=True,
+        do_xcom_push=False
+    )
+    mapproxy_generate_tiles_wm_light = KubernetesPodOperator(
+        name="mapproxy_generate_tiles_wm_light",
+        image="dsoapi.azurecr.io/mapproxy",
+        namespace="tiles",
+        arguments=["mapproxy-seed", "-c", "2", "-s", "/var/config/seed.yaml", "-f", "/var/config/mapproxy.yaml", "--seed=wm_kbk_light"],
+        task_id="mapproxy_generate_tiles_wm_light",
+        volumes=[volume_config_mapproxy, volume_data_mapproxy],
+        volume_mounts=[volume_mount_config, volume_mount_data_mapproxy],
+        security_context=dict(fsGroup=101),
+        node_selectors={"nodetype": "tiles"},
+        is_delete_operator_pod=True,
+        resources=fullresources,
+        get_logs=True,
+        do_xcom_push=False
+    )
     # mapproxy_generate_tiles_rd = KubernetesPodOperator(
     #     name="mapproxy_generate_tiles_rd",
     #     image="dsoapi.azurecr.io/mapproxy",
