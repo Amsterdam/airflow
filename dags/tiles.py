1f2c06a5f3f9018cf118fdf2a9bb4766f5c96392
Stephan Zaat <s.zaat@amsterdam.nl>
2021-02-18 09:11:16 -0400

    tiles_only

--- dags/tiles.py
+++ dags/tiles.py
@@ -357,11 +357,17 @@ with DAG(
         do_xcom_push=False
     )
 
-trex_generate_pbf_wm >> upload_pbf_wm
-trex_generate_pbf_rd >> upload_pbf_rd
-upload_pbf_wm >> mapproxy_generate_tiles_wm >> upload_tiles_wm
-upload_pbf_wm >> mapproxy_generate_tiles_wm_zw >> upload_tiles_wm_zw
-upload_pbf_wm >> mapproxy_generate_tiles_wm_light >> upload_tiles_wm_light 
-upload_pbf_rd >> mapproxy_generate_tiles_rd >> upload_tiles_rd 
-upload_pbf_rd >> mapproxy_generate_tiles_rd_zw >> upload_tiles_rd_zw
-upload_pbf_rd >> mapproxy_generate_tiles_rd_light >> upload_tiles_rd_light
+# trex_generate_pbf_wm >> upload_pbf_wm
+# trex_generate_pbf_rd >> upload_pbf_rd
+# upload_pbf_wm >> mapproxy_generate_tiles_wm >> upload_tiles_wm
+# upload_pbf_wm >> mapproxy_generate_tiles_wm_zw >> upload_tiles_wm_zw
+# upload_pbf_wm >> mapproxy_generate_tiles_wm_light >> upload_tiles_wm_light 
+# upload_pbf_rd >> mapproxy_generate_tiles_rd >> upload_tiles_rd 
+# upload_pbf_rd >> mapproxy_generate_tiles_rd_zw >> upload_tiles_rd_zw
+# upload_pbf_rd >> mapproxy_generate_tiles_rd_light >> upload_tiles_rd_light
+mapproxy_generate_tiles_wm >> upload_tiles_wm
+mapproxy_generate_tiles_wm_zw >> upload_tiles_wm_zw
+mapproxy_generate_tiles_wm_light >> upload_tiles_wm_light 
+mapproxy_generate_tiles_rd >> upload_tiles_rd 
+mapproxy_generate_tiles_rd_zw >> upload_tiles_rd_zw
+mapproxy_generate_tiles_rd_light >> upload_tiles_rd_light
