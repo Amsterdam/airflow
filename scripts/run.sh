4c6589ea18bbb0ed886120775149de74cb791ebb
bas <b.hendrikse@amsterdam.nl>
2021-07-26 15:30:51 +0200

    Fix for run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -23,8 +23,6 @@ fi
 
 args=("--pid" "$AIRFLOW_PID_FILE" "$@")
 
-export AIRFLOW__CORE__SQL_ALCHEMY_CONN=${AIRFLOW__CORE__SQL_ALCHEMY_CONN:-`echo $AIRFLOW_CONN_POSTGRES_DEFAULT | cut -d'?' -f 1`}
-export AIRFLOW_CONN_POSTGRES_VSD={$AIRFLOW_CONN_POSTGRES_VSD:-$AIRFLOW__CORE__SQL_ALCHEMY_CONN}
 airflow db init  # db init is not destructive, so can be re-run at startup
 airflow db upgrade  # upgrade DB if needed
 python scripts/mkvars.py
